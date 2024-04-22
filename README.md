77cd6f494ef3b5acdb07756ae88b1e79f873ea39
Marc-Antoine Duhaime <ma@duhaime.ca>
2024-04-22 09:50:32 -0400

    Version minimaliste du server

---
+++ README.md
@@ -0,0 +1,3 @@
+# Inspiration
+https://medium.com/@ctrlaltmonique/setting-up-an-express-typescript-server-with-vue-vite-9d415a51facc
+https://blog.logrocket.com/how-to-set-up-node-typescript-express/
---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PORT, NODE_ENV } = process.env;
+
+
+const app: Express = express();
+
+app.set('env', NODE_ENV);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
